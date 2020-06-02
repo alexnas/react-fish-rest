80fa3f6596587209dbf6331be1ac80000f10eaa7
alexnas <alexnas2python@gmail.com>
2020-06-02 10:46:33 +0300

    Just started. Create Store Picker.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/style.css';
+import StorePicker from './components/StorePicker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<StorePicker />, document.getElementById('root'));
