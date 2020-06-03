4db103b56147e0dc1f167bd2b2d3d97f7ff1b879
alexnas <alexnas2python@gmail.com>
2020-06-03 13:18:20 +0300

    Update and display Order State in order section (in the cart).

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 
+import { formatPrice } from '../helpers';
+
 class Order extends Component {
-  state = {};
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish && fish.status === 'available';
+    if (!isAvailable) {
+      return <li key={key}>Sorry, {fish.name} is no longer available</li>;
+    }
+    return (
+      <li key={key}>
+        {count} lbs {fish ? fish.name : 'fish'}{' '}
+        {formatPrice(fish.price * count)}
+      </li>
+    );
+  };
+
   render() {
-    return <div className='order'>Order</div>;
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      } else {
+        return prevTotal;
+      }
+    }, 0);
+    return (
+      <div className='order-wrap'>
+        <h2>Order</h2>
+        <ul className='order'>{orderIds.map(this.renderOrder)}</ul>
+        <div className='total'>
+          <strong>{formatPrice(total)}</strong>
+        </div>
+      </div>
+    );
   }
 }
 
